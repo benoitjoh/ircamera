8d221fc58f4dd727e1ee6dd33625b7be92780cb0
Johannes Benoit <jbenoit@t-online.de>
2021-01-14 23:31:54 +0100

    fix install, add a mask.pmg as example

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ircamera
+
+## to install on raspberrypi as user pi
+
+login as user pi
+
+
+git clone git@github.com:benoitjoh/ircamera.git
+
+cd ircamera
+
+sudo ./install.sh
+
+
--- install.sh
+++ install.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
+if [ "$(whoami)" != "pi" ]
+   then echo '[ERR] script must run in context of user pi!' && exit 1
+fi
+
+# copy some files
+cp conf/mask.pgm /home/pi
+cp source/lightcontrol.py /home/pi
+sudo cp conf/lightcontrol.service /etc/systemd/system
+
+
+
 
 # install motion
-sudo apt -y install motion htop
+sudo apt-get -y install motion htop
 sudo cp conf/motion.conf /etc/motion
- 
+
+# create outputdirectory and make it accessible for user motion
+pushd /var/lib
+if [ ! -d "motion" ]
+    then sudo mkdir motion
+fi
+sudo chmod 777 motion
+sudo chown motion motion
+cd motion
+sudo -u motion mkdir pict
+
+popd
+
+# create log directory and make it accessible for user motion
+pushd /var/log/
+if [ ! -d "motion" ]
+    then sudo mkdir motion
+fi
+sudo chmod 777 motion
+sudo chown motion motion
+
+popd
+
 # install the lichtcontrols
-cp source/lightcontrol.py ~/
-sudo cp conf/lightcontrol.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable lightcontrol.service
 sudo systemctl start lightcontrol.service
